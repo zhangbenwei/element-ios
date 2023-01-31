ccfa3699fc9bb5ae329fe1ceab43880272029260
Alfonso Grillo <alfogrillo@gmail.com>
2023-01-31 15:25:45 +0100

    Improve UT

--- RiotSwiftUI/Modules/Settings/Notifications/Test/Unit/NotificationSettingsViewModelTests.swift
+++ RiotSwiftUI/Modules/Settings/Notifications/Test/Unit/NotificationSettingsViewModelTests.swift
@@ -134,6 +134,10 @@ final class NotificationSettingsViewModelTests: XCTestCase {
             
             // The one to one room rule ui flag should match the loudest related poll rule
             XCTAssertEqual(self.viewModel.viewState.selectionState[.oneToOneRoom], true)
+        
+            // the oneToOneRoom rule should be flagged as "out of sync"
+            XCTAssertTrue(self.viewModel.isRuleOutOfSync(.oneToOneRoom))
+            XCTAssertFalse(self.viewModel.isRuleOutOfSync(.allOtherMessages))
             
             expectation.fulfill()
         }
